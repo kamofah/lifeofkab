7d6f6251b7592e6a457656a1a15f1474c3cb1a35
Kwaku Amofah-Boafo <boafo819@gmail.com>
2020-07-03 23:32:06 -0400

    use the DOM to control the about area animations and make them run smoother

--- script.js
+++ script.js
@@ -42,18 +42,52 @@ function typePhrase(){
 
 
 
+// Animation 
+let speakingHud = document.getElementById("speakingHud");
+let kaws = document.getElementById("kaws");
+let videogames = document.getElementById("videogames");
+let basketball = document.getElementById("ball");
+let photo = document.getElementById("photograph");
+let about = document.getElementById("about");
+about.addEventListener("click", () => {
+    setInterval(() => {
+        speakingHud.style.animationName = "slideIn";
+        speakingHud.style.animationDuration = "1s";
+        speakingHud.style.animationFillMode = "forwards";
+    } , 1000)
+    
+    setInterval(() => {
+        // controls animation for basketball image
+        basketball.style.visibility = "visible";
+        basketball.style.animationName = "slideIn";
+        basketball.style.webkitAnimationDuration = "1s";
+        basketball.style.webkitAnimationFillMode = "forwards";
+    } , 3000)
 
+    setInterval(() => {
+        // controls the animation for the camera image
+        photo.style.visibility = "visible";
+        photo.style.animationName = "slideUp";
+        photo.style.webkitAnimationDuration = "1s";
+        photo.style.webkitAnimationFillMode = "forwards";
+    }, 4000);
 
+    setInterval(() => {
+        // controls the animation for the Kaws image 
+        kaws.style.visibility = "visible";
+        kaws.style.animationName = "slideDown";
+        kaws.style.webkitAnimationDuration = "1s";
+        kaws.style.webkitAnimationFillMode = "forwards";
+    }, 5000);
 
+    setInterval(() => {
+        // controls the animation for the video games image
+        videogames.style.visibility = "visible";
+        videogames.style.animationName = "slideInL";
+        videogames.style.webkitAnimationDuration = "1s";
+        videogames.style.webkitAnimationFillMode = "forwards";
+    }, 6000);
 
-
-
-
-
-
-
-
-
-//
+})
 
 
